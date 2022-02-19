14971bce91a331a42c01561c6d19db5b80daa331
cjimenez <cjimenez@mdlab.com>
2022-02-19 15:23:52 -0500

    backup

---
+++ my-books/Data/AppDbInitlialer.cs
@@ -0,0 +1,54 @@
+﻿using my_books.Data.Models;
+
+namespace my_books.Data
+{
+    public class AppDbInitlialer
+    {
+        // This file initializes the database
+
+
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            // scope for the application services
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+                // Cehcking to see if we have any books in the database
+                if (!context.Books.Any())
+                {
+                    context.Books.AddRange(new Book()
+
+                    {
+                        Title = "new Book title",
+                        Description = "new Book Description",
+                        IsRead = true,
+                        DateRead = DateTime.Now.AddDays(-2),
+                        Rate = 4,
+                        Genre = "Biography",
+                        Author = "New Author",
+                        CoverUrl = "https...",
+                        DateAdded = DateTime.Now
+                    },
+    new Book()
+
+    {
+        Title = "other Book title",
+        Description = "other Book Description",
+        IsRead = true,
+        Genre = "Biography",
+        Author = "New Author",
+        CoverUrl = "https...",
+        DateAdded = DateTime.Now
+    });
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
--- my-books/Program.cs
+++ my-books/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using my_books.Data;
+ 
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Connection To db
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
+
 
 // Add services to the container.
 
@@ -9,17 +19,25 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+
+ 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CJ my_books v2"));
+    }
 
-app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.Run();
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllers();
+//});
+AppDbInitlialer.Seed(app); 
+  app.Run();
